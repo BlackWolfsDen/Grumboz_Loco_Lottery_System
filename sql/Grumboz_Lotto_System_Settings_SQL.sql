488f220a3b10e75c43c52906489b9153f2c23aa2
Grumbo <blackwolfsden@hotmail.com>
2014-08-01 19:07:44 -0600

    Update Grumboz_Lotto_System_Settings_SQL.sql

--- sql/Grumboz_Lotto_System_Settings_SQL.sql
+++ sql/Grumboz_Lotto_System_Settings_SQL.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.9-log - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             8.3.0.4694
+-- Host:                         localhost
+-- Server version:               5.6.10 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             8.3.0.4796
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,34 +17,34 @@ USE `lotto`;
 
 -- Dumping structure for table lotto.entries
 CREATE TABLE IF NOT EXISTS `entries` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `count` bigint(20) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `guid` int(11) unsigned NOT NULL COMMENT 'player db guid',
+  `count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table lotto.entries: ~5 rows (approximately)
+-- Dumping data for table lotto.entries: ~0 rows (approximately)
 /*!40000 ALTER TABLE `entries` DISABLE KEYS */;
-REPLACE INTO `entries` (`id`, `name`, `count`) VALUES
 /*!40000 ALTER TABLE `entries` ENABLE KEYS */;
 
 
 -- Dumping structure for table lotto.settings
 CREATE TABLE IF NOT EXISTS `settings` (
   `id` int(1) unsigned NOT NULL AUTO_INCREMENT,
-  `item` int(11) unsigned NOT NULL,
-  `timer` bigint(20) unsigned NOT NULL,
+  `item` int(11) unsigned DEFAULT NULL,
+  `timer` int(10) unsigned NOT NULL,
   `operation` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `mumax` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `comments` varchar(21844) NOT NULL,
-  KEY `id` (`id`)
+  `comments` varchar(21844) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 MAX_ROWS=1;
 
--- Dumping data for table lotto.settings: ~1 rows (approximately)
+-- Dumping data for table lotto.settings: ~0 rows (approximately)
 /*!40000 ALTER TABLE `settings` DISABLE KEYS */;
-REPLACE INTO `settings` (`id`, `item`, `timer`, `operation`, `mumax`, `comments`) VALUES
+INSERT INTO `settings` (`id`, `item`, `timer`, `operation`, `mumax`, `comments`) VALUES
 	(1, 44209, 1800000, 1, 10, 'item :\r\nthe item id for what they will win(custom currency)\r\n\r\ntimer :\r\n604800000 == 1 week\r\n86400000 == 1 day\r\n3600000 == 1 hour\r\n60000 == 1 minute');
 /*!40000 ALTER TABLE `settings` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+
